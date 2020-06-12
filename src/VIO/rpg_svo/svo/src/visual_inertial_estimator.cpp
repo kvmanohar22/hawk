b4d018eae721bc8df082ecf2c6346f721ea414a5
kvmanohar22 <kvmanohar22@gmail.com>
2020-06-12 17:24:08 +0530

    update

--- src/VIO/rpg_svo/svo/src/visual_inertial_estimator.cpp
+++ src/VIO/rpg_svo/svo/src/visual_inertial_estimator.cpp
@@ -353,7 +353,7 @@ void VisualInertialEstimator::rejectOutliers()
       point->last_outlier_check_id_ = opt_call_count_;
 
       bool point_status_removed=false;
-      for(Features::iterator it_obs=point->obs_.begin(); it_obs!=point->obs_.end();)
+      for(Features::iterator it_obs=point->obs_.begin(); it_obs!=point->obs_.end(); ++it_obs)
       {
         const size_t n_refs = point->nRefs();
         const Vector2d uv_true = (*it_obs)->px;
@@ -362,25 +362,12 @@ void VisualInertialEstimator::rejectOutliers()
 
         if(error > reprojection_threshold)
         {
-          ++n_removed_edges;
-          point_status_removed = true;
-
-          // IMPORTANT: we remove the current iterator from observations 
-          //            so don't advance iterator when removing an edge
-          const bool status = map_.removePtFrameRef((*it_obs)->frame, *it_obs);
-
-          // all the observations have been removed
-          if(status && n_refs <= 2)
-            break;
-
-          if(!status)
-            ++it_obs;
+          ++n_removed_points;
+          n_removed_edges += n_refs;
+          map_.safeDeletePoint(point);
+          break;
         }
-        else
-          ++it_obs;
       }
-      if(point_status_removed)
-        ++n_removed_points;
     }
   }
   SVO_DEBUG_STREAM("Estimator:" <<

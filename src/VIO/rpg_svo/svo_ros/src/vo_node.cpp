b3fac0661aacb0699c7af5ff68e7a08690b3e435
kvmanohar22 <kvmanohar22@gmail.com>
2020-06-22 04:50:15 +0530

    adding interface to control drone via visual estimates

--- src/VIO/rpg_svo/svo_ros/src/vo_node.cpp
+++ src/VIO/rpg_svo/svo_ros/src/vo_node.cpp
@@ -17,6 +17,7 @@
 #include <string>
 #include <svo/frame_handler_mono.h>
 #include <svo/map.h>
+#include <svo/frame.h>
 #include <svo/config.h>
 #include <svo/visual_inertial_estimator.h>
 #include <svo/inertial_initialization.h>
@@ -35,6 +36,7 @@
 #include <vikit/abstract_camera.h>
 #include <vikit/camera_loader.h>
 #include <vikit/user_input_thread.h>
+#include <std_srvs/Empty.h>
 
 namespace svo {
 
@@ -51,9 +53,15 @@ public:
   vk::AbstractCamera* cam1_;
   bool quit_;
   ros::Rate rate_;
-  InertialInitialization*      inertial_init_;
-  bool                         inertial_init_done_;
-  VoNode();
+  InertialInitialization* inertial_init_;
+  bool inertial_init_done_;
+  ros::NodeHandle nh_;
+  ros::Publisher mavros_pose_pub_; //!< Publishes pose estimates to mavros
+  bool publish_pose_estimates_;    //!< should we publish pose estimates to hawk
+  bool start_vo_;                  //!< Set this to start vo
+  ros::ServiceServer start_vo_server_; //!< create a service server
+
+  VoNode(ros::NodeHandle& nh);
   ~VoNode();
 
   void imuCb(const sensor_msgs::Imu::ConstPtr& msg);
@@ -66,17 +74,25 @@ public:
       const sensor_msgs::ImageConstPtr& l_msg,
       const sensor_msgs::ImageConstPtr& r_msg);
 
+  // publishes the current pose to drone
+  void publishPose(const FramePtr& frame);
+
+  // call this server to start vo
+  bool startVoServer(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response);
   bool initializeGravity();
 };
 
-VoNode::VoNode() :
+VoNode::VoNode(ros::NodeHandle& nh) :
   vo_(nullptr),
   publish_markers_(vk::getParam<bool>("/hawk/svo/publish_markers", true)),
   publish_dense_input_(vk::getParam<bool>("/hawk/svo/publish_dense_input", true)),
   cam_(nullptr),
   quit_(false),
   rate_(300),
-  inertial_init_done_(false)
+  inertial_init_done_(false),
+  nh_(nh),
+  publish_pose_estimates_(false),
+  start_vo_(vk::getParam<bool>("/hawk/svo/start_vo", true))
 {
   // Create Camera
   if(!vk::camera_loader::loadFromRosNs("svo", "cam0", cam_))
@@ -97,19 +113,50 @@ VoNode::VoNode() :
 
   inertial_init_ = new InertialInitialization(1.0, 0.03, Vector3d(0.0, 0.0, -9.807166));
 
+  // create publishers and services
+  mavros_pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped>(
+    "mavros/vision_pose/pose", 10);
+  start_vo_server_ = nh_.advertiseService("start_vo", &VoNode::startVoServer, this);
+
   // Init VO and start
   std::string rig_type(vk::getParam<std::string>("/hawk/svo/rig"));
   if(rig_type == "stereo")
     vo_ = new svo::FrameHandlerMono(cam_, cam1_, FrameHandlerBase::InitType::STEREO);
   else
     vo_ = new svo::FrameHandlerMono(cam_, cam1_);
-  vo_->start();
 }
 
 VoNode::~VoNode()
 {
   delete vo_;
   delete cam_;
+}
+
+void VoNode::publishPose(const FramePtr& frame)
+{
+  const SE3 T_w_b =  frame->T_f_w_.inverse() * FrameHandlerMono::T_c0_b_;
+  const Vector3d t = T_w_b.translation();
+  const Quaterniond q = T_w_b.unit_quaternion(); 
+
+  // FIXME: Need to set frame_id in header? 
+  geometry_msgs::PoseStamped pose;
+  pose.header.seq = frame->id_;
+  // pose.header.stamp = ros::Time::fromSec(frame->timestamp_);
+  pose.pose.position.x = t.x();
+  pose.pose.position.y = t.y();
+  pose.pose.position.z = t.z();
+  pose.pose.orientation.x = q.x();
+  pose.pose.orientation.y = q.y();
+  pose.pose.orientation.z = q.z();
+  pose.pose.orientation.w = q.w();
+
+  mavros_pose_pub_.publish(pose);
+}
+
+bool VoNode::startVoServer(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+  start_vo_ = true;
+  return true;
 }
 
 bool VoNode::initializeGravity()
@@ -173,6 +220,9 @@ void VoNode::imgCb(const sensor_msgs::ImageConstPtr& msg)
   // estimate motion
   vo_->addImage(img, msg->header.stamp);
 
+  if(publish_pose_estimates_)
+    publishPose(vo_->lastFrame());
+
   if(publish_markers_ && vo_->stage() != FrameHandlerBase::STAGE_PAUSED)
     visualizer_.visualizeMarkers(vo_->lastFrame(), vo_->coreKeyframes(), vo_->map());
 
@@ -209,6 +259,9 @@ void VoNode::imgStereoCb(
   // estimate motion
   vo_->addImage(l_img, r_img, l_msg->header.stamp);
 
+  if(publish_pose_estimates_)
+    publishPose(vo_->lastFrame());
+
   if(publish_markers_ && vo_->stage() != FrameHandlerBase::STAGE_PAUSED)
     visualizer_.visualizeMarkers(vo_->lastFrame(), vo_->coreKeyframes(), vo_->map());
 
@@ -226,7 +279,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "svo");
   ros::NodeHandle nh;
   std::cout << "create vo_node" << std::endl;
-  svo::VoNode vo_node;
+  svo::VoNode vo_node(nh);
 
   // subscribe to message topics
   std::string imu_topic(vk::getParam<std::string>("/hawk/svo/imu0/rostopic", "imu/data"));
@@ -257,6 +310,26 @@ int main(int argc, char **argv)
   } else {
     vo_node.inertial_init_done_ = true;
     vo_node.vo_->prior_pose_set_ = true;
+  }
+
+  if(vo_node.start_vo_)
+    vo_node.vo_->start();
+  else
+  {
+    // we wait until we expect a start
+    while(ros::ok())
+    {
+      ros::spinOnce();
+      if(vo_node.start_vo_)
+      {
+        SVO_WARN_STREAM("Start VO command received.");
+        vo_node.vo_->start();
+        break;
+      }
+      else
+        SVO_WARN_STREAM_THROTTLE(1.0, "Start VO not set.");
+      vo_node.rate_.sleep();
+    }
   }
 
   // start processing callbacks
